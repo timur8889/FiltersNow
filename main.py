e8e7208d8e18044cc52d5e3ed83b96a328460503
Timur <45330851+timur8889@users.noreply.github.com>
2025-10-07 00:56:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Токен бота (получите у @BotFather)
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
+BOT_TOKEN = 8278600298:AAFA-R0ql-dibAoBruxgwitHTx_LLx61OdM
 
 # Инициализация базы данных
 def init_db():
