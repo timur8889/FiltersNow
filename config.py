f8c36cf3daa67c79efb1a21cbfb2947481b60adc
Timur <45330851+timur8889@users.noreply.github.com>
2025-10-16 11:11:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,9 @@
+# config.py
 import os
+from typing import List
 
-# Токен бота
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
-
-# ID канала (начинается с @ или -100)
-CHANNEL_ID = os.getenv("CHANNEL_ID", "@your_channel_username")
-
-# ID администраторов
-ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "123456789").split(',')))
-
-# Настройки базы данных
-DATABASE_URL = os.getenv("DATABASE_URL", "channel_bot.db")
+class Config:
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "8278600298:AAGPjUhyU5HxXOaLRvu-FSRldBW_UCmwOME")
+    CHANNEL_ID = os.getenv("CHANNEL_ID", "@timur_onion")
+    ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "5024165375").split(",")]
+    DATABASE_URL = os.getenv("DATABASE_URL", "channel_bot.db")
